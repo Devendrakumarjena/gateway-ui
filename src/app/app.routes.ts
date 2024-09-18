1929675ae0291601b326daa0b5f6c2cddeed9cdb
Devendra kumar Jena <devendrajenatrichy@gmail.com>
2024-09-18 22:55:34 +0530

    models component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { DeviceComponent } from './components/device/device.component';
+import { ModulesComponent } from './components/modules/modules.component';
 
 export const routes: Routes = [
-    { path: 'device', component: DeviceComponent },
+  { path: 'device', component: DeviceComponent },
+  { path: 'modules', component: ModulesComponent },
   { path: '', redirectTo: '/device', pathMatch: 'full' }, // Default route
-  { path: '**', redirectTo: '/device' } 
+  { path: '**', redirectTo: '/device' },
 ];
