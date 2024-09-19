ab89560f361b1288e5d60f008102033b82601a82
Biswa ranjan patra <124027045+Biswaranjan6413@users.noreply.github.com>
2024-09-19 13:19:18 +0530

    connect routerlink  attributed tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLink ,RouterLinkActive } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, FooterComponent],
+  imports: [RouterOutlet, SidebarComponent, FooterComponent ,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
