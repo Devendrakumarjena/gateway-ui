5fbc14c686fe3859f2660dfb44b838516fe92fe5
Devendra kumar Jena <devendrajenatrichy@gmail.com>
2024-10-01 10:04:36 +0530

    inertface

--- src/app/components/wireless/wireless.component.ts
+++ src/app/components/wireless/wireless.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-wireless',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,FormsModule,NgFor],
   templateUrl: './wireless.component.html',
   styleUrl: './wireless.component.scss'
 })
 export class WIRELESSComponent implements OnInit {
   title: string = '';
+// accessPointSettings: any;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -19,4 +22,35 @@ export class WIRELESSComponent implements OnInit {
       this.title = data.title;
     });
   }
+
+   // WiFi network data model
+   wifiNetworks = [
+    {
+      ssid: 'HelloWorld2G',
+      password: '********',
+      encryption: 'WPA2-PSK',
+      ipMode: 'Static',
+      ipAddress: '192.168.1.21',
+      subnet: '255.255.255.0',
+      gateway: '192.168.1.1'
+    },
+    {
+      ssid: 'MyWifiNetwork',
+      password: '********',
+      encryption: 'WPA2-PSK',
+      ipMode: 'Dynamic',
+      ipAddress: '192.168.1.22',
+      subnet: '255.255.255.0',
+      gateway: '192.168.1.1'
+    }
+  ];
+
+  // Access point settings model
+  accessPointSettings = {
+    ssid: 'NeoA2e491',
+    password: '********',
+    subnet: '255.255.255.0',
+    dns1: '8.8.8.8',
+    dns2: '8.8.8.8'
+  };
 }
